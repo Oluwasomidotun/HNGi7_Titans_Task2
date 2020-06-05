576a1b5b68c0b50eeec755149839d2b0492e5648
Oluwasomidotun <53957792+Oluwasomidotun@users.noreply.github.com>
2020-06-06 00:05:17 +0100

    Added my file via uploads

---
+++ scripts/Oluwasomidotun.py
@@ -0,0 +1,5 @@
+def main ():
+    print ("Hello World, this is Oluwasomidotun Oladipupo with HNGi7 ID: HNG-06031 and Email: oluwasomidotun@outlook.com   using Python for stage 2 task")
+
+if __name__=="__main__":
+    main()
